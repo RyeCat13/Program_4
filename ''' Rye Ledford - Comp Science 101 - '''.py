731cb300d31963bedd6b1cdad03c873219af7e47
RyeCat13 <90850716+RyeCat13@users.noreply.github.com>
2021-10-01 20:55:05 -0500

    Program 4
    Rye Ledford

--- ''' Rye Ledford - Comp Science 101 - '''.py
+++ ''' Rye Ledford - Comp Science 101 - '''.py
@@ -7,85 +7,80 @@ if __name__ == '__main__':
     def main():
         spins = 1
 
-bank = int(input('How many chips do you want to start with? ==> '))
-
-def start_bank(begin_bank):
+while True:
     bank = int(input('How many chips do you want to start with? ==> '))
-    while True:
-        if bank <= 0:
-            print('Too low a value, you can only choose 1 - 100 chips')
-        elif bank >= 101:
-            print('Too high a value, you can only choose 1 - 100 chips')
-        else:
-            return True
-
-chips = int(input('How many chips do you want to wager? ==> '))
-
-def start_wager(betting):
-    while True:
-        for wager in range (-110, chips):
-            if (chips <= 0):
-                print('The wager amount must be greater than 0. Please enter again.')
-                break
-            elif (chips > bank):
-                print('The wager amount cannot be greater than how much you have.', bank)
-                break
-            else:
-                return total_wager
-
-reel1 = random.randint(1, 10)
-reel2 = random.randint(1, 10)
-reel3 = random.randint(1, 10) 
-
-def matches(reel1, reel2, reel3):
-    total_matches = 0 
-    if reel1 == reel2:
-        total_matches += 2
-    elif reel1 == reel3:
-        total_matches += 1
-    elif reel1 == reel3:
-        total_matches += 2
-    elif reel2 == reel3:
-        total_matches += 1
+    if bank <= 0:
+        print('Too low a value, you can only choose 1 - 100 chips')
+        continue
+    elif bank >= 101:
+        print('Too high a value, you can only choose 1 - 100 chips')
+        continue
     else:
-        total_matches = 0
-    return total_matches
-
-payout = 0
-total_matches = 0
-pay = 0
-total_payout = 0
-total_wager = 0
-
-def real_matches(payout):
-    if total_matches == 3:
-        pay == (chips * 10) - chips
-    elif total_matches == 2:
-        pay == (chips * 3) - chips
-    else:
-        pay == chips - (chips * 2)
-    return total_payout
-
-total_bank = pay + bank
-total_bank = bank - chips
-bank = total_bank
-
+        break
 
 while True:
-    '''chips = total_wager(bank)
-    reel1, reel2, reel3 = slot_print()'''
+    chips = int(input('How many chips do you want to wager? ==> '))
+    for wager in range (-110, chips):
+        if (chips <= 0):
+            print('The wager amount must be greater than 0. Please enter again.')
+            continue
+        elif (chips > bank):
+            print('The wager amount cannot be greater than how much you have.', bank)
+            continue
+        else:
+            break
 
-    total_matches = matches(reel1, reel2, reel3)
-    payout_won = real_matches(payout)
+    reel1 = random.randint(1, 10)
+    reel2 = random.randint(1, 10)
+    reel3 = random.randint(1, 10) 
 
-    print('Your reels were,', reel1, reel2, reel3)
-    print('You matched', real_matches(chips), 'reels')
-    print('Your won/lost', chips)
-    print('Current bank', bank)
-    print()
-    print('The most chips you had was', bank)
-    spins += 1
-    break
+    def matches(reel1, reel2, reel3):
+        total_matches = 0 
+        if reel1 == reel2:
+            total_matches += 2
+        elif reel1 == reel3:
+            total_matches += 1
+        elif reel1 == reel3:
+            total_matches += 2
+        elif reel2 == reel3:
+            otal_matches += 1
+        else:
+            total_matches = 0
+        return total_matches
+
+    payout = 0
+    total_matches = 0
+    pay = 0
+    total_payout = 0
+    total_wager = 0
+
+    def real_matches(payout):
+        if total_matches == 3:
+            pay == (chips * 10) - chips
+        elif total_matches == 2:
+            pay == (chips * 3) - chips
+        else:
+            pay == chips - (chips * 2)
+        return total_payout
+
+    total_bank = pay + bank
+    total_bank = bank - chips
+    bank = total_bank
+
+
+    while True:
+
+        total_matches = matches(reel1, reel2, reel3)
+        payout_won = real_matches(payout)
+
+        print('Your reels were,', reel1, reel2, reel3)
+        print('You matched', real_matches(chips), 'reels')
+        print('You won/lost', chips)
+        print('Current bank', bank)
+        print()
+        print('The most chips you had was', bank)
+        spins += 1
+        break
 
 
     while True:
@@ -97,7 +92,7 @@ while True:
         else:
             continue
 
-while False:
-    print('You lost all', bank, 'in', spins, 'spins')
+    while False:
+        print('You lost all', bank, 'in', spins, 'spins')
 
 main()
