51fe44b0cb0dc07b47ae4b32a6ec9750b3e4523b
RyeCat13 <90850716+RyeCat13@users.noreply.github.com>
2021-10-01 20:30:10 -0500

    Add files via upload
    My code and what I had completed. I've included four screen shots. I could not get this code to work so I tried a multitude of bug fixes. The screen shots show I was able to get the code working correctly for certain parts at one time but not the rest and vis, versa. I think the directions were very difficult to follow considering it referenced a starting code I was not given.

---
+++ ''' Rye Ledford - Comp Science 101 - '''.py
@@ -0,0 +1,103 @@
+'''Rye Ledford - Comp Science Lab 101 - Reels and bets'''
+
+import random 
+if __name__ == '__main__':
+    spins = 1
+
+    def main():
+        spins = 1
+
+bank = int(input('How many chips do you want to start with? ==> '))
+
+def start_bank(begin_bank):
+    bank = int(input('How many chips do you want to start with? ==> '))
+    while True:
+        if bank <= 0:
+            print('Too low a value, you can only choose 1 - 100 chips')
+        elif bank >= 101:
+            print('Too high a value, you can only choose 1 - 100 chips')
+        else:
+            return True
+
+chips = int(input('How many chips do you want to wager? ==> '))
+
+def start_wager(betting):
+    while True:
+        for wager in range (-110, chips):
+            if (chips <= 0):
+                print('The wager amount must be greater than 0. Please enter again.')
+                break
+            elif (chips > bank):
+                print('The wager amount cannot be greater than how much you have.', bank)
+                break
+            else:
+                return total_wager
+
+reel1 = random.randint(1, 10)
+reel2 = random.randint(1, 10)
+reel3 = random.randint(1, 10) 
+
+def matches(reel1, reel2, reel3):
+    total_matches = 0 
+    if reel1 == reel2:
+        total_matches += 2
+    elif reel1 == reel3:
+        total_matches += 1
+    elif reel1 == reel3:
+        total_matches += 2
+    elif reel2 == reel3:
+        total_matches += 1
+    else:
+        total_matches = 0
+    return total_matches
+
+payout = 0
+total_matches = 0
+pay = 0
+total_payout = 0
+total_wager = 0
+
+def real_matches(payout):
+    if total_matches == 3:
+        pay == (chips * 10) - chips
+    elif total_matches == 2:
+        pay == (chips * 3) - chips
+    else:
+        pay == chips - (chips * 2)
+    return total_payout
+
+total_bank = pay + bank
+total_bank = bank - chips
+bank = total_bank
+
+
+while True:
+    '''chips = total_wager(bank)
+    reel1, reel2, reel3 = slot_print()'''
+
+    total_matches = matches(reel1, reel2, reel3)
+    payout_won = real_matches(payout)
+
+    print('Your reels were,', reel1, reel2, reel3)
+    print('You matched', real_matches(chips), 'reels')
+    print('Your won/lost', chips)
+    print('Current bank', bank)
+    print()
+    print('The most chips you had was', bank)
+    spins += 1
+    break
+
+
+    while True:
+        user_input = input('Play again?\n')
+        if user_input == 'Y' or 'Yes':
+            main()
+        elif user_input == 'N' or 'No':
+            break
+        else:
+            continue
+
+while False:
+    print('You lost all', bank, 'in', spins, 'spins')
+
+main()
